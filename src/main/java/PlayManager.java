1a5277f722d8efab3769e278188583b57af5d1f8
Chime Njoku <chimeenjoku@gmail.com>
2018-10-04 18:07:52 -0500

    added incomplete S3 connection in Playermananger.java

---
+++ src/main/java/PlayManager.java
@@ -0,0 +1,100 @@
+import java.io.File;
+import java.io.IOException;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CreateBucketRequest;
+import com.amazonaws.services.s3.model.GetBucketLocationRequest;
+
+public class PlayManager {
+
+
+
+
+    /**
+     * <b>Important:</b> Be sure to fill in your AWS access credentials in
+     * ~/.aws/credentials (C:\Users\USER_NAME\.aws\credentials for Windows
+     * users) before you try to run this sample.
+     */
+    //TODO: add account credentials
+
+
+
+    public static void main(String[] args) throws IOException {
+        /*
+         * Create your credentials file at ~/.aws/credentials (C:\Users\USER_NAME\.aws\credentials for Windows users)
+         * and save the following lines after replacing the underlined values with your own.
+         *
+         * [default]
+         * aws_access_key_id = YOUR_ACCESS_KEY_ID
+         * aws_secret_access_key = YOUR_SECRET_ACCESS_KEY
+         */
+
+        String bucketName = "Risk_Game_Should_Be_Unique";
+        String clientRegion = "us-west-1";
+
+        System.out.println("===========================================");
+        System.out.println("Getting Started with Amazon S3");
+        System.out.println("===========================================\n");
+
+        try {
+            AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                    .withCredentials(new ProfileCredentialsProvider())
+                    .withRegion(clientRegion)
+                    .build();
+
+            if (!s3Client.doesBucketExistV2(bucketName)) {
+                // Because the CreateBucketRequest object doesn't specify a region, the
+                // bucket is created in the region specified in the client.
+                s3Client.createBucket(new CreateBucketRequest(bucketName));
+
+                // Verify that the bucket was created by retrieving it and checking its location.
+                String bucketLocation = s3Client.getBucketLocation(new GetBucketLocationRequest(bucketName));
+                System.out.println("Bucket location: " + bucketLocation);
+            }
+
+
+            System.out.println("Uploading a new object to S3 from a file\n");
+            s3.putObject(new PutObjectRequest(bucketName, key, createSampleFile()));
+
+
+
+            //TODO:Create a replay game function that downloads and reads file
+            /*
+             * Download an object - When you download an object, you get all of
+             * the object's metadata and a stream from which to read the contents.
+             * It's important to read the contents of the stream as quickly as
+             * possibly since the data is streamed directly from Amazon S3 and your
+             * network connection will remain open until you read all the data or
+             * close the input stream.
+             *
+             * GetObjectRequest also supports several other options, including
+             * conditional downloading of objects based on modification times,
+             * ETags, and selectively downloading a range of an object.
+             */
+            System.out.println("Downloading an object");
+            S3Object object = s3.getObject(new GetObjectRequest(bucketName, key));
+            System.out.println("Content-Type: "  + object.getObjectMetadata().getContentType());
+            displayTextInputStream(object.getObjectContent());
+
+        } catch (AmazonServiceException ase) {
+            System.out.println("Caught an AmazonServiceException, which means your request made it "
+                    + "to Amazon S3, but was rejected with an error response for some reason.");
+            System.out.println("Error Message:    " + ase.getMessage());
+            System.out.println("HTTP Status Code: " + ase.getStatusCode());
+            System.out.println("AWS Error Code:   " + ase.getErrorCode());
+            System.out.println("Error Type:       " + ase.getErrorType());
+            System.out.println("Request ID:       " + ase.getRequestId());
+        } catch (AmazonClientException ace) {
+            System.out.println("Caught an AmazonClientException, which means the client encountered "
+                    + "a serious internal problem while trying to communicate with S3, "
+                    + "such as not being able to access the network.");
+            System.out.println("Error Message: " + ace.getMessage());
+        }
+    }
+
+
+}
