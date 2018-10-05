d2c3f26274532981d186631bfe3b1a5fb897f0fa
ADMoreau <amoreau876@gmail.com>
2018-10-04 23:24:14 -0500

    Added test file

--- src/test/java/GameManagerTest.java
+++ src/test/java/GameManagerTest.java
@@ -82,7 +82,7 @@ public class GameManagerTest extends TestCase {
 
     @Test
     public void testGameManager() throws Exception {
-        public static String base = System.getProperty("user.dir");
+        String base = System.getProperty("user.dir");
 
         Object bm = new BoardManager(base + "\\src\\main\\java\\mapSource.json", base + "\\src\\main\\java\\deck.json");
         Object dice = new Dice();
